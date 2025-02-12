90283e4d9e98b515a5a8ba88d464e59c574a6c43
yoratoni <yoratoni.dev@gmail.com>
2025-02-12 02:09:05 +0100

    ...

--- app/controllers/templates/base_controller.ts
+++ app/controllers/templates/base_controller.ts
@@ -6,13 +6,15 @@ import { HttpContext } from "@adonisjs/core/http"
 export default class BaseController {
     constructor(protected ctx: HttpContext) {}
 
-    /**
-     * A private function to check data validity.
-     */
-    private _checkDataValidity(data: any) {
-        if (data === undefined || data === null) return false
-        return true
-    }
+	/**
+	 * A private method to check data validity.
+	 * @param data The data to be checked.
+     * @returns Whether the data is valid.
+	 */
+	private _checkDataValidity(data: unknown) {
+		if (data === undefined || data === null) return false
+		return true
+	}
 
     /**
      * Returns a properly formatted success response.
