292a80572ce9a867d1d974776fbb2d905bc27c3a
yoratoni <yoratoni.dev@gmail.com>
2025-06-20 13:33:32 +0200

    WIP

--- database/seeders/main/index_seeders.ts
+++ database/seeders/main/index_seeders.ts
@@ -20,7 +20,7 @@ export default class extends BaseSeeder {
 
     async run() {
         // Note that the order here is the order of seeding
-        await this.seed(await import("#database/seeders/roles_seeder"))
         await this.seed(await import("#database/seeders/users_seeder"))
+        await this.seed(await import("#database/seeders/roles_seeder"))
     }
 }
