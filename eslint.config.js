222d8e4cb9995bfd2d65e269caefc151e0098eae
yoratoni <yoratoni.dev@gmail.com>
2025-07-16 00:32:14 +0200

    ...

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,11 @@
-import { configApp } from "@adonisjs/eslint-config"
+import { configApp, INCLUDE_LIST, ADONIS_IGNORE_LIST, RULES_LIST } from "@adonisjs/eslint-config"
 
 export default configApp({
     ignores: [ "ace.js" ],
+    rules: {
+        ...RULES_LIST,
+        '@adonisjs/prefer-lazy-controller-import': [ 'error' ],
+        '@adonisjs/prefer-lazy-listener-import': [ 'error' ],
+        "@typescript-eslint/consistent-type-imports": [ "error" ],
+    },
 })
