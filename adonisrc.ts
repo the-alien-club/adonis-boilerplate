e8415c07d2c6581086ab13be01b7a723f918b33c
yoratoni <yoratoni.dev@gmail.com>
2025-09-16 23:17:19 +0200

    ...

--- adonisrc.ts
+++ adonisrc.ts
@@ -27,6 +27,7 @@ export default defineConfig({
         () => import("@adonisjs/cache/cache_provider"),
         () => import("@adonisjs/cors/cors_provider"),
         () => import("@adonisjs/lucid/database_provider"),
+        () => import("@adonisjs/limiter/limiter_provider"),
         () => import("@adonisjs/auth/auth_provider"),
         () => import("@adonisjs/bouncer/bouncer_provider"),
         () => import("@adonisjs/core/providers/vinejs_provider"),
