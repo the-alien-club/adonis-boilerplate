b6d1e0d04f78f9a86fb0a96ce8e0fbd4cf05b3d0
yoratoni <yoratoni.dev@gmail.com>
2025-09-25 21:22:03 +0200

    Added columnEncryptedJsonManager

--- lib/utils/columns.ts
+++ lib/utils/columns.ts
@@ -1,3 +1,4 @@
+import encryption from "@adonisjs/core/services/encryption"
 import type { ColumnOptions } from "@adonisjs/lucid/types/model"
 
 /**
@@ -5,27 +6,44 @@ import type { ColumnOptions } from "@adonisjs/lucid/types/model"
  */
 export const columnJsonManager = {
     prepare: (value: Record<string, any>) => {
+        if (value === null) return null
         if (typeof value === "string") return value
 
         let res: string | null = null
         try {
             res = JSON.stringify(value)
-        } catch (_) {
-            throw new Error("Failed to stringify JSON value")
+        } catch (error) {
+            throw new Error(`The Column JSON manager failed to stringify the value: ${value}. Error: ${error}`)
         }
 
         return res
     },
     consume: (value: string | Record<string, any>) => {
+        if (value === null) return null
         if (typeof value === "object" && value !== null) return value
 
         let res: Record<string, any> | null = null
         try {
             res = JSON.parse(value)
-        } catch (_) {
-            throw new Error("Failed to parse JSON value: " + value)
+        } catch (error) {
+            throw new Error(`The Column JSON manager failed to parse the value: ${value}. Error: ${error}`)
         }
 
         return res
+    },
+} satisfies Partial<ColumnOptions>
+
+/**
+ * Prepares and consumes JSON values for database columns, while also encrypting and decrypting them
+ * via the AdonisJS encryption service.
+ */
+export const columnEncryptedJsonManager = {
+    prepare: (value: Record<string, any>) => {
+        const preparedValue = columnJsonManager.prepare(value)
+        return preparedValue ? encryption.encrypt(preparedValue) : null
+    },
+    consume: (value: string) => {
+        const decryptedValue = encryption.decrypt(value)
+        return columnJsonManager.consume(decryptedValue as string)
     },
 } satisfies Partial<ColumnOptions>
